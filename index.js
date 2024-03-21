4436b0aed2ca4ea9124926b97f30f9f8a4c05501
FG <790859022@qq.com>
2024-03-21 15:41:03 +0800

    feat: config fabic component + c + arcts over

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // main index.js
-import ClipPath from 'react-native-clippathview/src/ClipPath'
-import ClipPathH from 'react-native-clippathview/src/ClipPathH'
-
-
-export const ClipPathViewH = ClipPathH
-export const ClipPathView = ClipPath
+// import ClipPath from 'react-native-clippathview/src/ClipPath'
+// import ClipPathH from 'react-native-clippathview/src/ClipPathH'
+// export const ClipPathViewH = ClipPathH
+// export const ClipPathView = ClipPath
+export * from './src/ClipPathNativeComponent'
